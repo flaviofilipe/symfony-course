86ad9c2adf7155f4ddd13e2836126438ae93560c
flaviofilipe <flaviofilipe97@gmail.com>
2020-02-11 16:22:22 -0300

    first commit

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\BlogPost;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
+/**
+ * @Route("/blog")
+ */
+class BlogController extends AbstractController
+{
+
+    /**
+     * @Route("/{page}", name="blog_list", defaults={"page":5}, requirements={"page"="\d+"})
+     */
+    public function list($page = 1, Request $request)
+    {
+        $limit = $request->get('limit', 10);
+        $repository = $this->getDoctrine()->getRepository(BlogPost::class);
+        $items = $repository->findAll();
+        return $this->json(
+            [
+                'page' => $page,
+                'limit' => $limit,
+                'data' => array_map(function (BlogPost $item) {
+                    return $this->generateUrl('blog_by_slug', ['slug' => $item->getSlug()]);
+                }, $items)
+            ]
+        );
+    }
+
+    /**
+     * @Route("/post/{id}", name="blog_by_id", requirements={"id"="\d+"}, methods={"GET"})
+     * @ParamConverter("post", class="App:BlogPost")
+     */
+    public function post(BlogPost $post)
+    {
+        return $this->json($post);
+    }
+
+    /**
+     * @Route("/post/{slug}", name="blog_by_slug", methods={"GET"})
+     * @ParamConverter("post", class="App:BlogPost",options={"mapping":{"slug":"slug"}})
+     */
+    public function postBySlug(BlogPost $post)
+    {
+        return $this->json($post);
+    }
+
+    /**
+     * @Route("/add", name="blog_add", methods={"POST"})
+     */
+    public function add(Request $request)
+    {
+        /** @var Serializer $serializer */
+        $serializer = $this->get('serializer');
+
+        $blogPost = $serializer->deserialize($request->getContent(), BlogPost::class, 'json');
+
+        $en = $this->getDoctrine()->getManager();
+        $en->persist($blogPost);
+        $en->flush();
+
+        return $this->json($blogPost);
+    }
+
+    /**
+     * @Route("/post/{id}", name="blog_delete", methods={"DELETE"})
+     */
+    public function delete(BlogPost $post)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($post);
+        $em->flush();
+
+        return $this->json(['message' => "Removido com sucesso!"]);
+    }
+}
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\BlogPost;
+use App\Entity\Comment;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+
+    /**
+     * @var UserPasswordEncoderInterface $passwordEncoder
+     */
+    private $passwordEncoder;
+
+    /**
+     * @var \Faker\Factory
+     */
+    private $faker;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+        $this->faker = \Faker\Factory::create();
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $this->loadUsers($manager);
+        $this->loadBlogPosts($manager);
+        $this->loadComments($manager);
+    }
+
+    public function loadBlogPosts(ObjectManager $manager)
+    {
+        $user = $this->getReference('user_admin');
+
+        for ($i = 0; $i < 100; $i++) {
+            $blog = new BlogPost();
+            $blog->setTitle($this->faker->realText(30));
+            $blog->setPublished($this->faker->dateTimeThisYear);
+            $blog->setContent($this->faker->realText());
+            $blog->setAuthor($user);
+            $blog->setSlug($this->faker->slug);
+            $this->setReference("blog_post_$i", $blog);
+
+            $manager->persist($blog);
+        }
+
+        $manager->flush();
+    }
+    public function loadComments(ObjectManager $manager)
+    {
+        for ($i=0; $i < 100; $i++) { 
+            for ($j=0; $j < rand(1, 10); $j++) { 
+                $comment = new Comment();
+                $comment->setContent($this->faker->realText());
+                $comment->setPublished($this->faker->dateTimeThisYear);
+                $comment->setAuthor($this->getReference('user_admin'));
+
+                $manager->persist($comment);
+            }
+        }
+
+        $manager->flush();
+
+    }
+
+    public function loadUsers(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('admin');
+        $user->setEmail('admin@blog.com');
+        $user->setName('Fx2');
+        $user->setPassword(
+            $this->passwordEncoder->encodePassword($user, '123456')
+        );
+
+        $this->addReference('user_admin', $user);
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
